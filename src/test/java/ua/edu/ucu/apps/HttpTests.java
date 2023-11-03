6061b348e5b8a8189ae295149f03810fb40bd72a
Eugene Bewz <bevz.pn@ucu.edu.ua>
2023-11-03 21:38:38 +0200

    Update yml file and moved test.http

--- src/test/java/ua/edu/ucu/apps/HttpTests.java
+++ src/test/java/ua/edu/ucu/apps/HttpTests.java
@@ -1,15 +1,20 @@
 package ua.edu.ucu.apps;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
 @SpringBootTest
 public class HttpTests {
 
-
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     @Test
-    void contextLoads() {
+    public void testYourEndpoint() {
+        String response = restTemplate.getForObject("/api/flowers", String.class);
+        // Add assertions to verify the response
     }
-
 }
