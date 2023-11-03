743cea1399a9fd3d2066d1bd9c7297c41037e894
Eugene Bewz <bevz.pn@ucu.edu.ua>
2023-11-03 22:25:36 +0200

    Added Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/flower_store_java-0.0.1-SNAPSHOT.jar
--- src/test/java/ua/edu/ucu/apps/FlowerStoreApplicationTests.java
+++ src/test/java/ua/edu/ucu/apps/FlowerStoreApplicationTests.java
@@ -2,8 +2,9 @@ package ua.edu.ucu.apps;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import ua.edu.ucu.apps.flowerstore.FlowerStoreApplication;
 
-@SpringBootTest
+@SpringBootTest(classes = FlowerStoreApplication.class)
 class FlowerStoreApplicationTests {
 
 	@Test
