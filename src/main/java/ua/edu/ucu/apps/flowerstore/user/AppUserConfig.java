a83200b3bc36c62ed74e050d0a261ed671d36886
Eugene Bewz <bevz.pn@ucu.edu.ua>
2023-11-13 12:29:44 +0200

    Commented CommandLineRunner for user until problem's resolved; changed links in test.http

--- src/main/java/ua/edu/ucu/apps/flowerstore/user/AppUserConfig.java
+++ src/main/java/ua/edu/ucu/apps/flowerstore/user/AppUserConfig.java
@@ -10,29 +10,29 @@ import java.util.List;
 
 @Configuration
 public class AppUserConfig {
-    @Bean
-    CommandLineRunner commandLineRunner(AppUserRepository repository) {
-        return args -> {
-            AppUser john_doe = new AppUser(
-                    "John Doe",
-                    "john.doe@gmail.com",
-                    LocalDate.of(2004, Month.DECEMBER, 30)
-            );
-
-            AppUser mary_sue = new AppUser(
-                    "Mary Sue",
-                    "mary.sue@gmail.com",
-                    LocalDate.of(2005, Month.DECEMBER, 30)
-            );
-
-            AppUser emily_rose = new AppUser(
-                    "Emily Rose",
-                    "emily.rose@gmail.com",
-                    LocalDate.of(2006, Month.DECEMBER, 30)
-            );
-            repository.saveAll(
-                    List.of(john_doe, mary_sue, emily_rose)
-            );
-        };
-    }
+//    @Bean
+//    CommandLineRunner commandLineRunner(AppUserRepository repository) {
+//        return args -> {
+//            AppUser john_doe = new AppUser(
+//                    "John Doe",
+//                    "john.doe@gmail.com",
+//                    LocalDate.of(2004, Month.DECEMBER, 30)
+//            );
+//
+//            AppUser mary_sue = new AppUser(
+//                    "Mary Sue",
+//                    "mary.sue@gmail.com",
+//                    LocalDate.of(2005, Month.DECEMBER, 30)
+//            );
+//
+//            AppUser emily_rose = new AppUser(
+//                    "Emily Rose",
+//                    "emily.rose@gmail.com",
+//                    LocalDate.of(2006, Month.DECEMBER, 30)
+//            );
+//            repository.saveAll(
+//                    List.of(john_doe, mary_sue, emily_rose)
+//            );
+//        };
+//    }
 }
